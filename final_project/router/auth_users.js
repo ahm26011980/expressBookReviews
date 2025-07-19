4d3f94fea9fe34b273d9d22914da1e56294f0dd7
ahm26011980 <ahm260180@gmail.com>
2025-07-19 15:58:01 -0400

    Task 8-9

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -29,7 +29,7 @@ regd_users.post("/login", (req,res) => {
 
   if (authenticatedUser(username, password)) {
     let accessToken = jwt.sign({
-      data: password
+      data: username
     }, 'access', { expiresIn: 60 * 60 });
 
     req.session.authorization = {
@@ -45,8 +45,108 @@ regd_users.post("/login", (req,res) => {
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  if (req.session.authorization) {
+    let token = req.session.authorization["accessToken"];
+    jwt.verify(token, "access", (err, user) => {
+        if (!err) {
+            // req.user = user;
+            
+            // Get review text
+            let reviewText = req.body.review;
+
+            // Get ISBN query
+            let isbn = req.params.isbn;
+
+            // get book object
+            const book = books[isbn];
+
+            // get reviews for user
+            if (hasUserReviewed(book, user)){
+                updateExistingReview(book, user, reviewText);
+            }
+            else{
+                addNewReview(book, user, reviewText);
+            }
+            return res.status(200).json({message: "Review added successfully"});
+        } else {
+            return res.status(403).json({ message: "User not authenticated" });
+        }
+    });
+  } else {
+    return res.status(403).json({ message: "User not logged in" });
+  }
 });
+
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+    if (req.session.authorization) {
+        let token = req.session.authorization["accessToken"];
+        jwt.verify(token, "access", (err, user) => {
+            if (!err) {
+                // req.user = user;
+                
+                // Get review text
+                let reviewText = req.body.review;
+    
+                // Get ISBN query
+                let isbn = req.params.isbn;
+    
+                // get book object
+                const book = books[isbn];
+    
+                // get reviews for user
+                if (hasUserReviewed(book, user)){
+                    deleteReview(book, user, reviewText);
+                }
+                
+                return res.status(200).json({message: "Reviews removed successfully."});
+            } else {
+                return res.status(403).json({ message: "User not authenticated" });
+            }
+        });
+      } else {
+        return res.status(403).json({ message: "User not logged in" });
+      }
+})
+
+function hasUserReviewed(book, user) {
+    const bookReviewList = Object.values(book.reviews);
+    const bookReview = bookReviewList.find(review => review.user === user.data);
+    
+    if (bookReview){
+        return true;
+    }
+    return false;
+}
+
+function addNewReview(book, user, reviewText) {    
+    book.reviews[Object.keys(book.reviews).length + 1] = {
+      user: user.data,
+      review: reviewText
+    };
+}
+
+function updateExistingReview(book, user, reviewText) {    
+    const bookReviewList = Object.values(book.reviews);
+    const bookReview = bookReviewList.find(review => review.user === user.data);
+
+    if (bookReview){
+        bookReview.review = reviewText;
+    }
+}
+
+function deleteReview(book, user) {
+    const reviews = book?.reviews;
+    if (!reviews) {
+        return;
+    }
+    
+    for (const reviewId in reviews) {
+        if (reviews[reviewId].user === user.data) {
+            delete reviews[reviewId];
+            return;
+        }
+    }
+}
 
 module.exports.authenticated = regd_users;
 module.exports.isValid = isValid;
